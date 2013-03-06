e4b352a503a940201b49eac042a4964cc1412097
山本和明 <yamamotokazuaki@yamamotokazuaki-no-MacBook-Pro.local>
2013-03-06 16:55:01 +0900

    charset

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 ini_set("display_errors" , true);
 error_reporting(E_ALL);
+
+header("Content-Type: text/html; Charset=UTF-8");
 	echo "this is an elastic netbeans test aaa2<br />"; 
 
 $dsn = "mysql:dbname=opstest1;host=ec2-54-249-134-89.ap-northeast-1.compute.amazonaws.com";
